d2feaba71af2be33ff658641a64a096b004b95d6
nwpelletier <nickw.pelletier@gmail.com>
2025-10-07 18:13:22 -0400

    Update ProjectCard.tsx
    added framer motion
    
    <motion.div> and params for animation

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 type ProjectCardProps = {
   title: string;
   information: string;
@@ -16,7 +18,13 @@ const ProjectCard = ({
   images,
 }: ProjectCardProps) => {
   return (
-    <div className="my-4 lg:my-8 border border-gray-100 overflow-hidden lg:h-96 flex flex-col lg:flex-row rounded-lg">
+    <motion.div
+      className="my-4 lg:my-8 border border-gray-100 overflow-hidden lg:h-96 flex flex-col lg:flex-row rounded-lg"
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.2 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+    >
       <div className="flex-[1] lg:h-full w-full lg:w-1/2 rounded-lg overflow-hidden flex flex-col pl-5 pr-5 py-10 gap-2 bg-white justify-center items-start md:pl-12">
         <h1 className="text-3xl tracking-wider font-bold">
           {title}
@@ -25,15 +33,17 @@ const ProjectCard = ({
           {information}
         </p>
         <p className="text-sm text-gray-500">{tech}</p>
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p className="text-sm text-gray-600 bg-white px-4 py-2 mt-5 ml-5 shadow-xs shadow-gray-400 rounded-3xl hover:bg-gray-400 hover:text-white transition-all duration-200">
-            {linkText}
-          </p>
-        </a>
+        {link && linkText && (
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-sm text-gray-600 bg-white px-4 py-2 mt-5 ml-5 shadow-xs shadow-gray-400 rounded-3xl hover:bg-gray-400 hover:text-white transition-all duration-200">
+              {linkText}
+            </p>
+          </a>
+        )}
       </div>
       <div className="flex-[1] lg:h-full w-full lg:w-1/2 flex flex-row flex-wrap justify-center items-center bg-gray-200 p-4 gap-1 overflow-auto">
         {images?.map((src, index) => (
@@ -45,7 +55,7 @@ const ProjectCard = ({
           />
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
