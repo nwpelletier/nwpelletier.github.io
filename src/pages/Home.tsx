56757a80df1cd6df432b2da8c8be9aa0da4f826b
nwpelletier <nickw.pelletier@gmail.com>
2025-10-07 13:51:06 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -60,7 +60,7 @@ export default function Home() {
                 rel="noopener noreferrer"
                 className="relative inline-block group whitespace-nowrap"
               >
-                Resume
+                View Resume
                 <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-gray-500 transition-all duration-300 group-hover:w-full"></span>
               </a>
             </div>
